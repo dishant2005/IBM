5a8013eaa7abacf37f8532efe8780d17d9217ac8
dishant2005 <147476749+dishant2005@users.noreply.github.com>
2024-09-17 15:11:07 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+#  1. Python
+#  2. R
+#  3. SQL
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - the tools used in Data Science
+# - Explain data visualization
+# - Use of R language
+# - Git and Github
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. Seaborn
+# 2. ggplot
+# 3. mateplot.lib
+
+# | Data Science Tools |
+# |--------------------|
+# | Jupyter Notebook   |
+# | RStudio            |
+# | Apache Zeppelin    |
+
+# ### arithmetic expression examples
+
+# In[2]:
+
+
+a=(3*4)+5
+a
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+b=  200/60
+b
+
+
+# ### Author
+# Dishant
+
+# In[ ]:
+
+
+
+
